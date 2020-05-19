a00fd5f747ed618875a755c6a41da65ee6e0eaf1
Noud de Brouwer <noud4@home.nl>
2020-05-20 00:05:09 +0200

    currency_converter dev

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', "CurrencyConverter@index");
+
+Route::post('/convert', "CurrencyConverter@convert");
